d4b9f0ecb152cdc6f92bf250719b78a1b14d5beb
Marcus <60523971+Simpada@users.noreply.github.com>
2022-11-19 16:16:40 +0100

    Basic site

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById("app"));
+function FrontPage() {
+    return <div>
+        <h1>Movie Database</h1>
+        <ul>
+            <li><Link to={"/movies"}>List Movies</Link></li>
+            <li><Link to={"/movies/new"}>Add New Movie</Link></li>
+        </ul>
+    </div>
+}
+
+function ListMovies() {
+    return null;
+}
+
+function AddNewMovie() {
+    return null;
+}
+
+function Application() {
+    return <BrowserRouter>
+        <Routes>
+            <Route path={"/"} element={<FrontPage />} />
+            <Route path={"/movies"} element={<ListMovies />} />
+            <Route path={"/movies/new"} element={<AddNewMovie />} />
+        </Routes>
+    </BrowserRouter>
+}
+
+ReactDOM.render(<Application />, document.getElementById("app"));
