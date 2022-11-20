e09df748334e56cbe1675cf14385bccb645e18a4
Marcus <60523971+Simpada@users.noreply.github.com>
2022-11-20 15:18:28 +0100

    Completed setting up jest testing for reactor

--- client/__tests__/listMovies.test.jsx
+++ client/__tests__/listMovies.test.jsx
@@ -1,6 +1,14 @@
+import {createRoot} from "react-dom/client";
+import * as React from "react";
+import {ListMovies} from "../pages/listMovies";
 
 describe("ListMovies component", () => {
     it ("shows loading screen", () => {
 
+        const domElement = document.createElement("div");
+        const root = createRoot(domElement);
+        root.render(<ListMovies />);
+
+        expect(domElement.innerHTML).toMatchSnapshot();
     })
 })
